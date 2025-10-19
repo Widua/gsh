d308e3f1349303d1bc6872fc5e50d2a31f6c6c50
Widua <mikolaj.widla@gmail.com>
2025-10-19 14:18:43 +0200

    codecrafters submit [skip ci]

--- app/commands/builtins.go
+++ app/commands/builtins.go
@@ -2,7 +2,9 @@ package commands
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -32,14 +34,53 @@ func EchoCommand(config *Config) error {
 }
 
 func TypeCommand(config *Config) error {
+	if len(config.Args) < 1 {
+		return fmt.Errorf("This command require at least one parameter")
+	}
+	cmdName := config.Args[0]
 
-	_, exists := Commands[config.Args[0]]
+	_, exists := Commands[cmdName]
 
-	if !exists {
-		fmt.Printf("%v: not found\n", config.Args[0])
+	if exists {
+
+		fmt.Printf("%v is a shell builtin\n", cmdName)
 		return nil
 	}
 
-	fmt.Printf("%v is a shell builtin\n", config.Args[0])
+	path := os.Getenv("PATH")
+
+	splittedPath := strings.Split(path, ":")
+
+	for _, pathenv := range splittedPath {
+		foundPath := searchPath(pathenv, cmdName)
+		if foundPath != "" {
+			fmt.Printf("%v is %v\n", cmdName, foundPath)
+			return nil
+		}
+
+	}
+
+	fmt.Printf("%v: not found\n", cmdName)
+
 	return nil
+}
+
+func searchPath(path string, cmdName string) string {
+	found := ""
+	walkfn := func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return filepath.SkipDir
+		}
+		if info.Name() == cmdName && !info.IsDir() && strings.Contains(info.Mode().String(), "x") {
+			found = path
+			return filepath.SkipAll
+		}
+		return nil
+	}
+	filepath.Walk(path, walkfn)
+	if len(found) > 0 {
+		return found
+	}
+	return ""
+
 }
